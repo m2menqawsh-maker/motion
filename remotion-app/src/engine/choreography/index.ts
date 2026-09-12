952a1411131c3706da8e6fd76b946a459a0c67cc
momen <momen@example.com>
2026-09-12 20:22:55 +0300

    refactor(templates): purge legacy templates from remotion-app to enforce data-driven architecture

--- remotion-app/src/engine/choreography/index.ts
+++
@@ -1,4 +0,0 @@
-export { resolveWindowPose } from "./resolveWindowPose";
-export type { WindowPose } from "./resolveWindowPose";
-export { mapCursorPath } from "./mapCursorPath";
-export { filterCursorPath } from "./filterCursorPath";
--- remotion-app/src/engine/scenes/index.ts
+++
@@ -1,5 +0,0 @@
-export { ChaosDesktop } from "./ChaosDesktop";
-export { ProductReveal } from "./ProductReveal";
-export { FeatureShowcase } from "./FeatureShowcase";
-export { HeadlineResolution } from "./HeadlineResolution";
-export { Closer } from "./Closer";
